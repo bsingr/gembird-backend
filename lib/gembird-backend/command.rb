3a5efb70cf3a438fd9685ad44e264555e04dd624
Jens Bissinger <mail@jens-bissinger.de>
2012-04-01 17:45:00 +0200

    added some more documentation

--- lib/gembird-backend/command.rb
+++ lib/gembird-backend/command.rb
@@ -29,26 +29,48 @@ module GembirdBackend
     end
     
     # scans for devices
+    # @return [Array<Hash>] devices
+    # @example return
+    #     [{:number=>"0",
+    #       :usb_device=>"001",
+    #       :usb_bus=>"004",
+    #       :type=>"4-socket SiS-PM",
+    #       :serial=>"02:01:48:af:e0"}]
     def devices
       call '-s'
     end
     
     # switch a socket on
+    # @return [Array<Hash>] devices
+    # @see #status example return values
     def on! socket='all', options={}
       call '-o', socket, options
     end
     
     # switch a socket off
+    # @return [Array<Hash>] devices
+    # @see #status example return values
     def off! socket='all', options={}
       call '-f', socket, options
     end
     
     # toggle a socket
+    # @return [Array<Hash>] devices
+    # @see #status example return values
     def toggle! socket='all', options={}
       call '-t', socket, options
     end
     
     # get the status of a socket
+    # @return [Array<Hash>] devices
+    # @example return
+    #     [{:number=>"0",
+    #       :usb_device => "003",
+    #       :sockets=>{"1"=>"on",
+    #                  "2"=>"off",
+    #                  "3"=>"on",
+    #                  "4"=>"off"}}]
+    #       :sockets=>{"1"=>"on", "2"=>"off", "3"=>"on", "4"=>"off"}}]
     def status socket='all', options={}
       call '-g', socket, options
     end
